ece4eb5c66a021c92d3abf83664dc19ffeb457b3
huyhoangng <133932405+huyhoangng@users.noreply.github.com>
2023-05-25 09:23:36 +0700

    Add files via upload

--- mang ex1.c
+++ mang ex1.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
 void main()
 {
-    int ary[15];
-    int i , total,high;
-    for(i=0;i<15;i++)
+    int ary[5];
+    int i , high,min;
+    float avg;
+    int total=0;
+    for(i=0;i<5;i++)
     {
+        printf("Enter number:\n");
         scanf("%d",&ary[i]);
+        total += ary[i];
+        if (i==0) {
+            high = ary[0];
+            min = ary[0];
+        }
+        else
+        if (ary[i] > high)
+            high = ary[i];
+        if (ary[i] < min)
+            min = ary[i];
     }
-    high=ary[0];
-    for (i=1;i<15;i++)
-    {
-        if(ary[i]>high)
-            high=ary[i];
-    }
+    avg= (float)total/i;
     printf("\nHighest value entered was %d",high);
+    printf("\nMin value entered was %d",min);
+    printf("\nAVG=%f",avg);
+    printf ("\nTotal= %d",total);
 }
